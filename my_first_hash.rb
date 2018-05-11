3746c80750f1e84887d413fa2510761c476212ed
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-11 18:07:40 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,7 +16,7 @@ def shipping_manifest
 end
 
 def retrieval
-  shipping_manifest["oil porcelain"]
+  the_manifest["oil porcelain"]
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
